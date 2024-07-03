47722dab3f4d7afdf984efb4bbf5a28a31aa3e9d
Meng-Che Lee <bluesky19970313@gmail.com>
2024-07-04 00:24:53 +0800

    3191

--- 3191_minOperations/3191_minOperation.cpp
+++ 3191_minOperations/3191_minOperation.cpp
@@ -22,3 +22,21 @@ public:
         return operation_count;
     }
 };
+// or
+//O(logn)
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0;
+        int right = arr.size()-k;
+
+        while(left < right){
+            //mid is the leftmost element of sliding window
+            //mid+k is the rightmost element 
+            int mid = left + (right-left)/2;
+            if(x - arr[mid] > arr[mid+k] - x)left = mid+1;
+            else right = mid;
+        }
+        return vector<int> (arr.begin()+left,arr.begin()+left+k);
+    }
+};
